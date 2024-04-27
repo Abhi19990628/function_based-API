ad875067b335b3274b7995c51c4edfcc1951725a
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-28 02:58:08 +0530

    Update views.py

--- fbvapp2/views.py
+++ fbvapp2/views.py
@@ -1,6 +1,4 @@
-/
-
-
+#import form django and restframework
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -8,19 +6,31 @@ from rest_framework import status
 from .models import Schools
 from .serializer import SchoolSerializer
 
+
+#appling method on requests---------------------------------------------------,..
+
+
 @api_view(['GET', 'POST'])
 def SchoolListView(request):
     if request.method == 'GET':
         school = Schools.objects.all()
         schoolSerializer = SchoolSerializer(school, many=True)
         return Response(schoolSerializer.data)
+
+    
     elif request.method == 'POST':
         schoolSerializer = SchoolSerializer(data=request.data)
+        
         if schoolSerializer.is_valid():
             schoolSerializer.save()
             return Response(schoolSerializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(schoolSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# appling method by primary key --------------------------------------------------------,.....
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def SchooldetailsView(request, pk):
@@ -32,6 +42,8 @@ def SchooldetailsView(request, pk):
     if request.method == 'GET':
         schoolSerializer = SchoolSerializer(school)
         return Response(schoolSerializer.data)
+
+    
     elif request.method == 'PUT':
         schoolSerializer = SchoolSerializer(school, data=request.data)
         if schoolSerializer.is_valid():
@@ -39,6 +51,8 @@ def SchooldetailsView(request, pk):
             return Response(schoolSerializer.data)
         else:
             return Response(schoolSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
     elif request.method == 'DELETE':
         school.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
