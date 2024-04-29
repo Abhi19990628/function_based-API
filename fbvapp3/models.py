742393584d068b662528d599d1f31a288125e1d1
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-30 02:43:08 +0530

    Update models.py

--- fbvapp3/models.py
+++ fbvapp3/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Student(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50),
     fathername=models.CharField(max_length=50)
     mothername=models.CharField(max_length=50)
     phone_no=models.IntegerField()
