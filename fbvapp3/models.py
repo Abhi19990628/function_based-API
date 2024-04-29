73062c4dea117844a512343f6b3c300504f31ddd
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-30 02:44:22 +0530

    Update models.py

--- fbvapp3/models.py
+++ fbvapp3/models.py
@@ -4,5 +4,5 @@ class Student(models.Model):
     name=models.CharField(max_length=50)
     fathername=models.CharField(max_length=50)
     mothername=models.CharField(max_length=50)
-    phone_no=models.IntegerField(),
+    phone_no=models.IntegerField()
     
